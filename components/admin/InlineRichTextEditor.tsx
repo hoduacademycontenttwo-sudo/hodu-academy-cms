68a009f4feacf4c78f4a0f92b54a1ccba957123b
victoriousvijay <victoriousvijay04@gmail.com>
2026-07-07 14:30:13 +0530

    auto: update 7/7/2026, 2:30:13 pm

--- components/admin/InlineRichTextEditor.tsx
+++ components/admin/InlineRichTextEditor.tsx
@@ -1,14 +1,13 @@
 'use client'
 
 import { useEditor, EditorContent } from '@tiptap/react'
-import Document from '@tiptap/extension-document'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
-import Bold from '@tiptap/extension-bold'
-import Italic from '@tiptap/extension-italic'
-import History from '@tiptap/extension-history'
-import TextStyle from '@tiptap/extension-text-style'
-import Color from '@tiptap/extension-color'
+import { Document } from '@tiptap/extension-document'
+import { Paragraph } from '@tiptap/extension-paragraph'
+import { Text } from '@tiptap/extension-text'
+import { Bold } from '@tiptap/extension-bold'
+import { Italic } from '@tiptap/extension-italic'
+import { TextStyle } from '@tiptap/extension-text-style'
+import { Color } from '@tiptap/extension-color'
 import { Bold as BoldIcon, Italic as ItalicIcon } from 'lucide-react'
 
 interface InlineRichTextEditorProps {
